9bec85fd9dfd9e3caa2f6a7e80fb1f391b39d639
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-06-25 16:32:49 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>🚀 Next.js App Deployed on EC2</h1>
+      <p>This is a simple Next.js app for testing Docker + EC2 deployment.</p>
+    </div>
+  );
+}
